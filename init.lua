25fbfe00cc05f7ba017630a0346b4c082ed1d5bf
Renvaderz <ningninghsu309@gmail.com>
2024-02-02 11:08:30 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+local options = {
+    cmdheight = 2,
+    clipboard = "unnamedplus",
+    mouse = "a",
+    undofile = true,
+    tabstop = 4,
+    shiftwidth = 4,
+    cursorline=true,
+    }
+    for k, v in pairs(options) do
+	    vim.opt[k] =v
+    end
+    local keymap =vim.api.nvim_set_keymap
+    local opts = {noremap = true, silent = true}
+
+
